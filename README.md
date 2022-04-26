0a68b1bed35f819c119b0177f34a545e4fd0d74f
Mathias Iversen <work@mathiasiversen.com>
2022-04-27 00:30:15 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+https://github.com/poem-web/poem/blob/master/poem-lambda/src/lib.rs
+https://github.com/trillium-rs/trillium/blob/main/aws-lambda/src/lib.rs
---
+++ src/context.rs
@@ -0,0 +1,36 @@
+use lambda_http::lambda_runtime::Context;
+use std::ops::Deref;
+
+pub(crate) struct LambdaContext(Context);
+impl LambdaContext {
+    pub fn new(context: Context) -> Self {
+        Self(context)
+    }
+}
+
+impl Deref for LambdaContext {
+    type Target = Context;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+/**
+Provides access to the aws lambda context for [`trillium::Conn`].
+
+See [`lamedh_runtime::Context`] for more details on the data available
+on this struct.
+*/
+pub trait LambdaConnExt {
+    /// returns the [`lamedh_runtime::Context`] for this conn
+    fn lambda_context(&self) -> &Context;
+}
+
+impl LambdaConnExt for trillium::Conn {
+    fn lambda_context(&self) -> &Context {
+        &*self
+            .state::<LambdaContext>()
+            .expect("lambda context should always be set inside of a lambda server")
+    }
+}
